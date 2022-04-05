c2a16847cc764585f5e57ed26a0a2985b839bf26
Gera Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2022-04-05 00:56:03 -0500

    Create Caballo.py

---
+++ Caballo.py
@@ -0,0 +1,7 @@
+#Creas tu objeto tipo caballo
+class caballo:
+    def __init__(self, tiempo, nombre):
+        self.tiempo = tiempo
+        self.nombre = nombre
+        self.izquierda = None
+        self.derecha = None 
